bdb90a1a6d7edecf06076c805fd802bf009c81b1
huynguyenngocdang <danghuy9229@gmail.com>
2023-12-14 10:53:38 +0700

    Revert "login"
    This reverts commit 01acd3d4ec3d87219d8555d64d37e7f19c48fa24.

--- cg-lazada-plus-project/src/main/java/com/codegym/cglazadaplusproject/controller/UserServlet.java
+++ cg-lazada-plus-project/src/main/java/com/codegym/cglazadaplusproject/controller/UserServlet.java
@@ -3,8 +3,6 @@ package com.codegym.cglazadaplusproject.controller;
 import com.codegym.cglazadaplusproject.dao.IUserDAO;
 import com.codegym.cglazadaplusproject.dao.UserDAO;
 import com.codegym.cglazadaplusproject.model.User;
-import com.codegym.cglazadaplusproject.service.UserLoginValidator;
-import com.codegym.cglazadaplusproject.service.Validator;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -41,7 +39,7 @@ public class UserServlet extends HttpServlet {
         int userId = Integer.parseInt(  request.getParameter("userId"));
         User selectUser = userDAO.getUserById(userId);
         try {
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/view/users/edit.jsp");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/view/edit.jsp");
             request.setAttribute("selectUser", selectUser);
             dispatcher.forward(request, response);
         } catch (ServletException | IOException e) {
@@ -53,7 +51,7 @@ public class UserServlet extends HttpServlet {
         List<User> users = userDAO.getAllUser();
         request.setAttribute("users", users);
         try {
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/view/users/list.jsp");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/view/list.jsp");
             dispatcher.forward(request, response);
         } catch (ServletException | IOException e) {
             e.printStackTrace();
@@ -71,31 +69,8 @@ public class UserServlet extends HttpServlet {
             case "edit":
                 editUser(request, response);
                 break;
-            case "login":
-                login(request, response);
             default:
                 break;
-        }
-    }
-
-    private void login(HttpServletRequest request, HttpServletResponse response) {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        Validator validator = new UserLoginValidator(username, password);
-        try {
-            String message = null;
-            if (validator.isCheck()) {
-                message = "Login successfully";
-
-            } else {
-                message = "Wrong username or password";;
-            }
-            request.setAttribute("message", message);
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/view/index.jsp");
-            dispatcher.forward(request, response);
-
-        } catch (ServletException | IOException e) {
-            e.printStackTrace();
         }
     }
 
@@ -110,7 +85,7 @@ public class UserServlet extends HttpServlet {
             selectUser = userDAO.getUserById(userId);
             request.setAttribute("selectUser", selectUser);
 
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/view/users/edit.jsp");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/view/edit.jsp");
             dispatcher.forward(request, response);
         } catch (ServletException | IOException e) {
             e.printStackTrace();
