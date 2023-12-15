7146d07e0eedcc7311c680b32cdca67b3b27c1e3
huynguyenngocdang <danghuy9229@gmail.com>
2023-12-15 15:14:15 +0700

    display category, product in index page

--- cg-lazada-plus-project/src/main/java/com/codegym/cglazadaplusproject/constant/Query.java
+++ cg-lazada-plus-project/src/main/java/com/codegym/cglazadaplusproject/constant/Query.java
@@ -6,4 +6,6 @@ public interface Query {
     String get_user_by_name = "Select * from user where user_name = ?";
     String update_user_by_id =
             "UPDATE `user` SET `user_name` = ?, `user_password` = ? WHERE (`user_id` = ?);";
+    String get_all_category = "Select * from category WHERE is_delete = ? ";
+    String get_all_product = "Select * from product WHERE is_delete = 0";
 }
